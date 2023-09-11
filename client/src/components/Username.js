dfd70d42e0997145d7efa48218378024b0e58d2d
Supriyo Saha <mm.saha1982@gmail.com>
2023-09-11 20:23:12 +0530

    user name validator added using formik and toaster

--- client/src/components/Username.js
+++ client/src/components/Username.js
@@ -1,11 +1,27 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import avatar from "../assets/profile.png";
-import styles from '../styles/Username.module.css';
+import styles from "../styles/Username.module.css";
+import { Toaster } from "react-hot-toast";
+import { useFormik } from "formik";
+import { usernameValidate } from "../helper/validate";
 
 const Username = () => {
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+    },
+    validate: usernameValidate,
+    validateOnBlur: false,
+    validateOnChange: false,
+    onSubmit: async (values) => {
+      console.log(values);
+    },
+  });
+
   return (
     <div class="container mx-auto">
+      <Toaster position="top-center" reverseOrder={false}></Toaster>
       <div className="flex justify-center items-center h-screen">
         <div className={styles.glass}>
           <div className="title flex flex-col item-center">
@@ -15,14 +31,21 @@ const Username = () => {
             </span>
           </div>
 
-          <form className="py-1">
+          <form className="py-1" onSubmit={formik.handleSubmit}>
             <div className="profile flex justify-center py-4">
               <img src={avatar} alt="avatar" className={styles.profile_img} />
             </div>
 
             <div className="textbox flex flex-col items-center gap-6">
-              <input className={styles.textbox} type="text" placeholder="Username" />
-              <button className={styles.btn} type="submit">Lets Go</button>
+              <input
+                {...formik.getFieldProps("username")}
+                className={styles.textbox}
+                type="text"
+                placeholder="Username"
+              />
+              <button className={styles.btn} type="submit">
+                Lets Go
+              </button>
             </div>
 
             <div className="text-center py-4">
