aaf2582d19d30a64bec2c7b3ae1d773e3ed3d852
Supriyo Saha <mm.saha1982@gmail.com>
2023-09-11 19:33:08 +0530

    login ui complete

--- client/src/components/Username.js
+++ client/src/components/Username.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import avatar from "../assets/profile.png";
+import styles from '../styles/Username.module.css';
 
 const Username = () => {
   return (
-    <div>
-      Username
-    </div>
-  )
-}
+    <div class="container mx-auto">
+      <div className="flex justify-center items-center h-screen">
+        <div className={styles.glass}>
+          <div className="title flex flex-col item-center">
+            <h1 className="text-5xl font-bold text-center">Hello Again</h1>
+            <span className="py-4 text-xl  text-center text-gray-500">
+              Explore more by connecting with us
+            </span>
+          </div>
 
-export default Username
+          <form className="py-1">
+            <div className="profile flex justify-center py-4">
+              <img src={avatar} alt="avatar" className={styles.profile_img} />
+            </div>
+
+            <div className="textbox flex flex-col items-center gap-6">
+              <input className={styles.textbox} type="text" placeholder="Username" />
+              <button className={styles.btn} type="submit">Lets Go</button>
+            </div>
+
+            <div className="text-center py-4">
+              <span className="text-gray-500">
+                Not a Member
+                <Link className="text-red-500 ml-2" to="/register">
+                  Register Now
+                </Link>
+              </span>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Username;
