f8336ed4ffef45a7fb762daf9bda719d736252ff
Supriyo Saha <mm.saha1982@gmail.com>
2023-09-13 21:38:46 +0530

    mongodb connected

--- server/server.js
+++ server/server.js
@@ -1,26 +1,32 @@
-import express from 'express';
-import cors from 'cors';
-import morgan from 'morgan';
-
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import connect from "./database/conn.js";
 
 const app = express();
 
 //middleware
 app.use(express.json());
 app.use(cors());
-app.use(morgan('tiny'));
-app.disable('x-powered-by'); // less hackers know about the stack
-
+app.use(morgan("tiny"));
+app.disable("x-powered-by"); // less hackers know about the stack
 
 const port = 8080;
 
 //http get request
-app.get('/', (req,res) => {
-    res.status(201).json("Home GET request");
+app.get("/", (req, res) => {
+  res.status(201).json("Home GET request");
 });
 
-
-//start server
-app.listen(port, () => {
-    console.log(`server is connected to http://localhost:${port}`);
+//start server only when have the valid connection
+connect().then(() => {
+  try {
+    app.listen(port, () => {
+      console.log(`server is connected to http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.log("can not connect to the server");
+  }
+}).catch(error => {
+    console.log("Invalid database connection..!", error);
 })
