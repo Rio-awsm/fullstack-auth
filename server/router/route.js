ae16c28fd41abd48b53947746b938ff5f5bc2231
Supriyo Saha <mm.saha1982@gmail.com>
2023-09-14 16:10:40 +0530

    otp generate and verification created

--- server/router/route.js
+++ server/router/route.js
@@ -3,7 +3,7 @@ const router = Router();
 
 /** import all controllers */
 import * as controller from "../controllers/appController.js";
-import Auth from "../middleware/auth.js";
+import Auth, { localVariables } from "../middleware/auth.js";
 
 //post methods
 router.route("/register").post(controller.register); //register user
@@ -13,7 +13,9 @@ router.route("/login").post(controller.verifyUser, controller.login); //login
 
 //get methods
 router.route("/user/:username").get(controller.getUser); //user with username
-router.route("/generateOTP").get(controller.generateOTP); //random otp
+router
+  .route("/generateOTP")
+  .get(controller.verifyUser, localVariables, controller.generateOTP); //random otp
 router.route("/verifyOTP").get(controller.verifyOTP); //verify generated otp
 router.route("/createResetSession").get(controller.createResetSession); //reset all the variables
 
