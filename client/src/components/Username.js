5ada9ea3d32e09cba14abad785ee8bb118280ff6
Supriyo Saha <mm.saha1982@gmail.com>
2023-09-14 20:37:42 +0530

    zustand store created for fetching username

--- client/src/components/Username.js
+++ client/src/components/Username.js
@@ -5,8 +5,13 @@ import styles from "../styles/Username.module.css";
 import { Toaster } from "react-hot-toast";
 import { useFormik } from "formik";
 import { usernameValidate } from "../helper/validate";
+import { useAuthStore } from "../store/store";
 
 const Username = () => {
+
+  const navigate = useNavigate();
+  const setUsername = useAuthStore(state => state.setUsername);
+
   const formik = useFormik({
     initialValues: {
       username: "",
@@ -15,7 +20,8 @@ const Username = () => {
     validateOnBlur: false,
     validateOnChange: false,
     onSubmit: async (values) => {
-      console.log(values);
+      setUsername(values.username);
+      navigate('/password')
     },
   });
 
