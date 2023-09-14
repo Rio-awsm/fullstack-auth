f00aeeb32c67c4e54ded238e25621614858ba480
Supriyo Saha <mm.saha1982@gmail.com>
2023-09-14 23:12:30 +0530

    successfull profile updation

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -2,28 +2,39 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import avatar from "../assets/profile.png";
 import styles from "../styles/Username.module.css";
-import { Toaster } from "react-hot-toast";
+import toast, { Toaster } from "react-hot-toast";
 import { useFormik } from "formik";
 import { profileValidation } from "../helper/validate";
 import convertToBase64 from "../helper/convert";
 import extend from "../styles/Profile.module.css";
+import useFetch from "../hooks/fetch.hook";
+import { updateUser } from "../helper/helper";
 
 const Profile = () => {
   const [file, setfile] = useState();
+  const [{ isLoading, apiData, serverError }] = useFetch();
+  const navigate = useNavigate();
   const formik = useFormik({
     initialValues: {
-      firstname: "",
-      lastname: "",
-      mobile: "",
-      email: "",
-      address: "",
+      firstName: apiData?.firstName || "",
+      lastName: apiData?.lastName || "",
+      email: apiData?.email || "",
+      mobile: apiData?.mobile || "",
+      address: apiData?.address || "",
     },
+    enableReinitialize: true,
     validate: profileValidation,
     validateOnBlur: false,
     validateOnChange: false,
     onSubmit: async (values) => {
       values = await Object.assign(values, { profile: file || "" });
-      console.log(values);
+      let updatePromise = updateUser(values);
+
+      toast.promise(updatePromise, {
+        loading: "Updating...",
+        success: <b>Update Successfully...!</b>,
+        error: <b>Could not Update!</b>,
+      });
     },
   });
 
@@ -32,15 +43,29 @@ const Profile = () => {
     setfile(base64);
   };
 
+  // logout handler function
+  function userLogout() {
+    localStorage.removeItem("token");
+    navigate("/");
+  }
+
+  if (isLoading) return <h1 className="text-2xl font-bold">isLoading</h1>;
+  if (serverError)
+    return <h1 className="text-xl text-red-500">{serverError.message}</h1>;
+
   return (
-    <div class="container mx-auto">
+    <div className="container mx-auto">
       <Toaster position="top-center" reverseOrder={false}></Toaster>
-      <div className="flex justify-center items-center h-auto py-8 ">
-        <div className={`${styles.glass} ${extend.glass}`} style={{ width: "45%" }}>
-          <div className="title flex flex-col item-center">
-            <h1 className="text-5xl font-bold text-center">Profile</h1>
-            <span className="py-4 text-xl  text-center text-gray-500">
-              You can Update the Details.
+
+      <div className="flex justify-center items-center h-auto py-7">
+        <div
+          className={`${styles.glass} ${extend.glass}`}
+          style={{ width: "45%", paddingTop: "3em" }}
+        >
+          <div className="title flex flex-col items-center">
+            <h4 className="text-5xl font-bold">Profile</h4>
+            <span className="py-4 text-xl w-2/3 text-center text-gray-500">
+              You can update the details.
             </span>
           </div>
 
@@ -48,50 +73,51 @@ const Profile = () => {
             <div className="profile flex justify-center py-4">
               <label htmlFor="profile">
                 <img
-                  src={file || avatar}
-                  alt="avatar"
+                  src={apiData?.profile || file || avatar}
                   className={`${styles.profile_img} ${extend.profile_img}`}
+                  alt="avatar"
                 />
               </label>
+
               <input
+                onChange={onUpload}
                 type="file"
                 id="profile"
                 name="profile"
-                onChange={onUpload}
               />
             </div>
 
             <div className="textbox flex flex-col items-center gap-6">
-            <div className="name flex w-3/4 gap-10">
-            <input
-                {...formik.getFieldProps("firstname")}
-                className={`${styles.textbox} ${extend.textbox}`}
-                type="text"
-                placeholder="First Name"
-              />
-              <input
-                {...formik.getFieldProps("lastname")}
-                className={`${styles.textbox} ${extend.textbox}`}
-                type="text"
-                placeholder="Last Name"
-              />
-            </div>
+              <div className="name flex w-3/4 gap-10">
+                <input
+                  {...formik.getFieldProps("firstName")}
+                  className={`${styles.textbox} ${extend.textbox}`}
+                  type="text"
+                  placeholder="FirstName"
+                />
+                <input
+                  {...formik.getFieldProps("lastName")}
+                  className={`${styles.textbox} ${extend.textbox}`}
+                  type="text"
+                  placeholder="LastName"
+                />
+              </div>
 
-            <div className="name flex w-3/4 gap-10">
-            <input
-                {...formik.getFieldProps("mobile")}
-                className={`${styles.textbox} ${extend.textbox}`}
-                type="text"
-                placeholder="Mobile number"
-              />
-              <input
-                {...formik.getFieldProps("email")}
-                className={`${styles.textbox} ${extend.textbox}`}
-                type="email"
-                placeholder="E-mail"
-              />
-            </div>
-              
+              <div className="name flex w-3/4 gap-10">
+                <input
+                  {...formik.getFieldProps("mobile")}
+                  className={`${styles.textbox} ${extend.textbox}`}
+                  type="text"
+                  placeholder="Mobile No."
+                />
+                <input
+                  {...formik.getFieldProps("email")}
+                  className={`${styles.textbox} ${extend.textbox}`}
+                  type="text"
+                  placeholder="Email*"
+                />
+              </div>
+
               <input
                 {...formik.getFieldProps("address")}
                 className={`${styles.textbox} ${extend.textbox}`}
@@ -105,10 +131,10 @@ const Profile = () => {
 
             <div className="text-center py-4">
               <span className="text-gray-500">
-                Come back later?
-                <Link className="text-red-500 ml-2" to="/">
+                come back later?{" "}
+                <button onClick={userLogout} className="text-red-500" to="/">
                   Logout
-                </Link>
+                </button>
               </span>
             </div>
           </form>
