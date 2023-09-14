9085f93cb80161007b2e971f837b98ce15c00be1
Supriyo Saha <mm.saha1982@gmail.com>
2023-09-14 23:51:47 +0530

    successfull reset password

--- client/src/components/Reset.js
+++ client/src/components/Reset.js
@@ -1,11 +1,20 @@
-import React from "react";
-
-import styles from "../styles/Username.module.css";
-import { Toaster } from "react-hot-toast";
+import React, { useEffect } from "react";
+import toast, { Toaster } from "react-hot-toast";
 import { useFormik } from "formik";
 import { resetPasswordValidation } from "../helper/validate";
+import { resetPassword } from "../helper/helper";
+import { useAuthStore } from "../store/store";
+import { useNavigate, Navigate } from "react-router-dom";
+import useFetch from "../hooks/fetch.hook";
+
+import styles from "../styles/Username.module.css";
 
 const Reset = () => {
+  const { username } = useAuthStore((state) => state.auth);
+  const navigate = useNavigate();
+  const [{ isLoading, apiData, status, serverError }] =
+    useFetch("createResetSession");
+
   const formik = useFormik({
     initialValues: {
       password: "",
@@ -15,35 +24,52 @@ const Reset = () => {
     validateOnBlur: false,
     validateOnChange: false,
     onSubmit: async (values) => {
-      console.log(values);
+      let resetPromise = resetPassword({ username, password: values.password });
+
+      toast.promise(resetPromise, {
+        loading: "Updating...",
+        success: <b>Reset Successfully...!</b>,
+        error: <b>Could not Reset!</b>,
+      });
+
+      resetPromise.then(function () {
+        navigate("/password");
+      });
     },
   });
 
+  if (isLoading) return <h1 className="text-2xl font-bold">isLoading</h1>;
+  if (serverError)
+    return <h1 className="text-xl text-red-500">{serverError.message}</h1>;
+  if (status && status !== 201)
+    return <Navigate to={"/password"} replace={true}></Navigate>;
+
   return (
-    <div class="container mx-auto">
+    <div className="container mx-auto">
       <Toaster position="top-center" reverseOrder={false}></Toaster>
+
       <div className="flex justify-center items-center h-screen">
-        <div className={styles.glass}>
-          <div className="title flex flex-col item-center">
-            <h1 className="text-5xl font-bold text-center">Reset Password</h1>
-            <span className="py-4 text-xl  text-center text-gray-500">
-              Enter New Password
+        <div className={styles.glass} style={{ width: "50%" }}>
+          <div className="title flex flex-col items-center">
+            <h4 className="text-5xl font-bold">Reset</h4>
+            <span className="py-4 text-xl w-2/3 text-center text-gray-500">
+              Enter new password.
             </span>
           </div>
 
-          <form className="pt-20" onSubmit={formik.handleSubmit}>
+          <form className="py-20" onSubmit={formik.handleSubmit}>
             <div className="textbox flex flex-col items-center gap-6">
               <input
                 {...formik.getFieldProps("password")}
                 className={styles.textbox}
-                type="password"
+                type="text"
                 placeholder="New Password"
               />
               <input
                 {...formik.getFieldProps("confirm_pwd")}
                 className={styles.textbox}
-                type="password"
-                placeholder="Confirm Password"
+                type="text"
+                placeholder="Repeat Password"
               />
               <button className={styles.btn} type="submit">
                 Reset
@@ -55,5 +81,4 @@ const Reset = () => {
     </div>
   );
 };
-
 export default Reset;
--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -222,8 +222,7 @@ export async function verifyOTP(req, res) {
 
 export async function createResetSession(req, res) {
   if (req.app.locals.resetSession) {
-    req.app.locals.resetSession = false; //allow access to this route only once
-    return res.status(201).send({ msg: "access granted" });
+    return res.status(201).send({ flag : req.app.locals.resetSession})
   }
   return res.status(440).send({ error: "Session expired" });
 }
